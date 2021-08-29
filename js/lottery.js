4675861507008cba7a8ff61d643667485a49266c
TTTyj <444596335@qq.com>
2021-08-30 00:13:42 +0800

    update windowMessage

---
+++ js/lottery.js
@@ -0,0 +1,11 @@
+document.getElementById('modal_content').style.display = "none";
+function start() {
+    document.getElementById('modal_content').style.display = "";
+}
+function hide() {
+    document.getElementById('modal_content').style.display = "none";
+}
+function jumpToGainList(){
+    var url="../html/awardList.html"
+    location.replace(url);
+}
